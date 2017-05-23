9c9bc930bb22b146f91748ebaab92768c0c0ae24
jfolleas1 <jacques.folleas@insa-lyon.fr>
2017-05-23 16:33:36 +0200

    2 first sections

--- monController.js
+++ monController.js
@@ -1,30 +1,48 @@
 var app = angular.module('monActe', []);
 app.controller('monCtrl', function($scope) {
 
-    $scope.divDOptIsVisible1 = function() {
-        if ($scope.monOption1) {
+    // initialisation de toutes les variables
+    $scope.codeActe = 123;
+
+    var aujd = new Date();
+    $scope.annee = aujd.getFullYear();
+
+    $scope.notaire = {
+        prenom: "Phillipe",
+        nom: "Antoine",
+    }
+
+    $scope.office = {
+        nom: "Phillipe",
+        residence: "Antoine",
+        codePostale: "Phillipe",
+        adresse: "Antoine",
+    }
+
+
+    $scope.affichageSection50128 = function() {
+
+        if ($scope.Choix50125 == "Sous seing privé") {
             return true;
         } else {
             return false;
         }
     }
+    $scope.affichageSection50126 = function() {
 
-    $scope.divDOptIsVisible2 = function() {
-        if ($scope.monOption2) {
+        if ($scope.Choix50125 == "Authentique") {
             return true;
         } else {
             return false;
         }
     }
+    $scope.affichageSection50130 = function() {
 
-    $scope.ConditionIsVisible = function() {
-
-        if ($scope.divDOptIsVisible1() && $scope.divDOptIsVisible2()) {
+        if ($scope.Choix50125 == "Brevet") {
             return true;
         } else {
             return false;
         }
     }
-
 
 });
